583bcb4646cc2d2293ccbc9e83e13424846e7de4
Adhit Herwansyah <adhit.herwansyah@pactindo.com>
2023-03-13 16:01:28 +0700

    Add Shared Preferences

--- app/src/main/java/com/adhit/androidcourse/lifecycle/MainActivity.java
+++ app/src/main/java/com/adhit/androidcourse/lifecycle/MainActivity.java
@@ -2,13 +2,65 @@ package com.adhit.androidcourse.lifecycle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText komentarText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        komentarText = findViewById(R.id.komentar_text);
+    }
+
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    protected void onStart() {
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT);
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT);
+        //Save Data Komentar
+        saveDataKomentar(komentarText.getText().toString());
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT);
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT);
+        //Ambil Data Komentar yang tersimpoan
+        String dataKomentar = ambilDataKomentar();
+        komentarText.setText(dataKomentar);
+        super.onResume();
+    }
+
+    private void saveDataKomentar(String value){
+        SharedPreferences.Editor dataKomentar = this.getSharedPreferences("komentar", Context.MODE_PRIVATE).edit();
+        dataKomentar.putString("keyKomentar", value);
+        dataKomentar.commit();
+    }
+
+    private String ambilDataKomentar(){
+        String result = this.getSharedPreferences("komentar", Context.MODE_PRIVATE).getString("keyKomentar", null);
+        return result;
     }
 }
